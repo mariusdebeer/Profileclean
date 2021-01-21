8fd9b04c556629a09a2e2c513c8b52ce2ea071b9
Marius de Beer <53187487+mariusdebeer@users.noreply.github.com>
2021-01-21 10:39:56 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -23,17 +23,37 @@ $Items = @('Archived History',
         $ChromeFolder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\Default"
         $ChromeFolder2 = "$($env:USERPROFILE)\chrome_data\Chrome\User Data\Default"
 
+ 
+
         # set min age of files
         $max_age = “-90”
+
+ 
 
         # get the current date
         $curr_date = Get-Date
 
+ 
+
         # determine how far back we go based on current date
         $del_date = $curr_date.AddDays($max_age)
 
+ 
+
         # delete only .ost files which are older than 90 days
         Get-ChildItem -include *.ost $user_path -Recurse | 
+        Where-Object {$_.LastWriteTime -lt $del_date} |
+            Where-Object { -not ($_.psiscontainer) } | 
+            Foreach-Object {Remove-Item $_.FullName}
+
+         # delete only .crdownload files which are older than 90 days
+        Get-ChildItem -include *.crdownload $user_path -Recurse | 
+        Where-Object {$_.LastWriteTime -lt $del_date} |
+            Where-Object { -not ($_.psiscontainer) } | 
+            Foreach-Object {Remove-Item $_.FullName}
+
+        # delete only .bak files which are older than 90 days
+        Get-ChildItem -include *.bak $user_path -Recurse | 
         Where-Object {$_.LastWriteTime -lt $del_date} |
             Where-Object { -not ($_.psiscontainer) } | 
             Foreach-Object {Remove-Item $_.FullName}
